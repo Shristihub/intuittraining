3ccfaca9d4904a942c644933459a762d2adc58a1
shristitechacademy <sripriyamathan@gmail.com>
2025-05-06 14:34:16 +0530

    gateway api

---
+++ ecom-gateway-api/src/test/java/com/gateway/EcomGatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomGatewayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
